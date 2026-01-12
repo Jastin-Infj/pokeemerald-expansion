055775589570de40841aa478db8997cb7fccaf02
Jastin-Infj <takumiyokoo@gmail.com>
2026-01-12 15:43:09 +0900

    DexNav: remove movement penalties for shaking grass and update v1.52 docs

--- src/dexnav.c
+++ src/dexnav.c
@@ -1199,46 +1199,9 @@ static void Task_DexNavSearch(u8 taskId)
 {
     struct Task *task = &gTasks[taskId];
 
-    if (sDexNavSearchDataPtr->proximity > MAX_PROXIMITY)
-    { // out of range
-        if (sDexNavSearchDataPtr->hiddenSearch && !task->tRevealed)
-            EndDexNavSearch(taskId);
-        else
-            EndDexNavSearchSetupScript(EventScript_LostSignal, taskId);
-        return;
-    }
-
-    if (sDexNavSearchDataPtr->proximity <= CREEPING_PROXIMITY && !gPlayerAvatar.creeping && task->tFrameCount > 60)
-    { //should be creeping but player walks normally
-        if (sDexNavSearchDataPtr->hiddenSearch && !task->tRevealed)
-            EndDexNavSearch(taskId);
-        else
-            EndDexNavSearchSetupScript(EventScript_MovedTooFast, taskId);
-        return;
-    }
-
-    if (sDexNavSearchDataPtr->proximity <= SNEAKING_PROXIMITY && TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_DASH | PLAYER_AVATAR_FLAG_BIKE))
-    { // running/biking too close
-        if (!(DEXNAV_IGNORE_SPEED_PENALTY || FlagGet(FLAG_RANDOMIZER_DEBUG_LOG)))
-        {
-            //always do event script, even if player hasn't revealed a hidden mon. It's assumed they would be creeping towards it
-            EndDexNavSearchSetupScript(EventScript_MovedTooFast, taskId);
-            return;
-        }
-    }
-
     if (ArePlayerFieldControlsLocked() == TRUE)
     { // check if script just executed
         EndDexNavSearch(taskId);
-        return;
-    }
-
-    if (gTasks[taskId].tFrameCount > DEXNAV_TIMEOUT * 60)
-    { // player took too long
-        if (sDexNavSearchDataPtr->hiddenSearch && !task->tRevealed)
-            EndDexNavSearch(taskId);
-        else
-            EndDexNavSearchSetupScript(EventScript_PokemonGotAway, taskId);
         return;
     }
 
